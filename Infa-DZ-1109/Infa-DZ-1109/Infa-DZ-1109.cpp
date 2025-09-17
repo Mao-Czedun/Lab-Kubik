ada87a266f92202198fcb786481c187a88c7dc3a
Mao-Czedun <egorbalagura7@gmail.com>
2025-09-17 10:01:17 +0300

    Add files via upload

---
+++ Infa-DZ-1109/Infa-DZ-1109/Infa-DZ-1109.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<clocale>
+#include<cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	double a, b, c, x, f;
+	cout << "Введите последовательно: a,b,c,x" << endl;
+	cin >> a >> b >> c >> x;
+	if (x < 3 && b != 0)
+		cout << (a * x * x - b * x + c);
+	else
+		if ((x > 3) && (b == 0))
+			cout << ((x - a) / (x - c));
+		else
+			if (x == 3)
+				cout << (x / c);
+}
